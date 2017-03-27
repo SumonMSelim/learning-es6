6050f05298d419ef5a7e629c440338c0242425fa
Muhammad Sumon Molla Selim <me@sumonselim.com>
2017-03-27 17:23:19 +0600

    creating my own es6 module

--- es6-modules/app.js
+++ es6-modules/app.js
@@ -1,6 +1,8 @@
-import { uniq } from 'lodash';
-import insane from 'insane';
-import jsonp from 'jsonp';
+import {uniq} from 'lodash';
+import {apiKey as key, url, sayHi} from './src/config';
+
+sayHi('Sumon');
+console.log(key, url);
 
 const ages = [15, 20, 67, 52, 12, 12];
 console.log(uniq(ages));
---
+++ es6-modules/src/config.js
@@ -0,0 +1,10 @@
+// Default Export, can be named anything - can only be one per module
+// export default apiKey;
+
+// Named Export
+export const apiKey = 'abc123';
+export const url = 'https://www.sumonselim.com/wp-json/wp/v2';
+
+export function sayHi(name) {
+    console.log(`Hello there ${name}`);
+}
